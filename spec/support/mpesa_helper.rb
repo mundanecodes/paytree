12305f95eac536c3b218fe91ff697e176ffc85c3
charles chuck <chalcchuck@gmail.com>
2025-07-14 06:15:40 +0300

    adds shared(MpesaHelper) to reduce setup

---
+++ spec/support/mpesa_helper.rb
@@ -0,0 +1,19 @@
+RSpec.shared_context "Mpesa config" do
+  before do
+    stub_request(:get, %r{/oauth/v1/generate}).to_return(
+      status: 200,
+      body: {access_token: "test_token", expires_in: 3600}.to_json,
+      headers: {"Content-Type" => "application/json"}
+    )
+
+    Payments.configure(:mpesa, Payments::Configs::Mpesa) do |config|
+      config[:key] = "TEST_KEY"
+      config[:secret] = "TEST_SECRET"
+      config[:shortcode] = "600999"
+      config[:passkey] = "PASSKEY"
+      config[:sandbox] = true
+      config[:extras] = {callback_url: "https://example.com/callback"}
+      config[:adapter] = Payments::Mpesa::Adapters::Daraja
+    end
+  end
+end
